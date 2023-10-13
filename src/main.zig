d9864c448ff9baf31da3bbb8862166f945e7bf43
Florian Liesegang <lysigk@gmail.com>
2023-10-13 11:37:30 +0200

    read input from stdin

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,19 @@
 const std = @import("std");
 
+const INPUT_WIDTH = 786;
+const INPUT_HEIGHT = 588;
+
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const stdin = std.io.getStdIn();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var buf: [INPUT_WIDTH * INPUT_HEIGHT * 3]u8 = undefined;
+    var n: usize = undefined;
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    while (true) {
+        n = try stdin.read(&buf);
 
-    try bw.flush(); // don't forget to flush!
-}
+        std.debug.print("{b}", .{buf});
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+        if (n == 0) break;
+    }
 }
